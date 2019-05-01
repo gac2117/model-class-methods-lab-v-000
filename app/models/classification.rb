0def9efc039c7058bebd7d7ec6e6628fa2387644
Grace Choi <oiograceoio@gmail.com>
2019-05-01 14:04:48 +0000

    Automatically backed up by Learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -7,6 +7,6 @@ class Classification < ActiveRecord::Base
   end
 
   def self.longest
-    Boat.where(length: Classification.includes(:boats).maximum("length"))
+    boat = Boat.where(length: Classification.includes(:boats).maximum("length"))
   end
 end
